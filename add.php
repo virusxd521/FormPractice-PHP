7ea75f5db5bbe085e6b0cd9c3f21c9eceeb784cb
Daniel <virusxd521@gmail.com>
2022-02-13 19:57:11 +0200

    fixed issues with quotes

--- add.php
+++ add.php
@@ -23,7 +23,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel=stylesheet href="./add.css">
+    <link rel="stylesheet" href="./add.css">
 </head>
 <body>
     <form class="forms" action="./add.php" method="POST">
--- choosingEdit.php
+++ choosingEdit.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel=stylesheet href="./choosingEdit.css">
+    <link rel="stylesheet" href="./choosingEdit.css">
 </head>
 <body>
     <form class="main__form" method="POST" action="./edit.php">
